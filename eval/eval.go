a9f367617f975eb42e3553168efd6d9a6f1f12da
kfigon <kamil.figon@gmail.com>
2025-12-22 22:57:17 +0100

    more work

--- eval/eval.go
+++ eval/eval.go
@@ -4,6 +4,16 @@ import "go-lisp/models"
 
 // return last evaluated expression
 func Eval(ast []models.SExpression) (models.SExpression, error) {
+	e := &evaluator{
+		rootEnv: models.NewEnv(nil),
+	}
+	return e.eval(ast)
+}
 
+type evaluator struct {
+	rootEnv *models.Env
+}
+
+func (e *evaluator) eval(ast []models.SExpression) (models.SExpression, error) {
 	return ast[0], nil
 }
